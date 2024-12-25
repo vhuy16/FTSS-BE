4c33281e02e3e083c27a52a5848b18b600e92038
vhuy16 <109493886+vhuy16@users.noreply.github.com>
2024-12-25 15:37:35 +0700

    .

--- FTSS/FTSS_API/Payload/Request/User/CreateNewAccountRequest.cs
+++ FTSS/FTSS_API/Payload/Request/User/CreateNewAccountRequest.cs
@@ -12,5 +12,6 @@ public class CreateNewAccountRequest
     public string Email { get; set; }
     public GenderEnum Gender { get; set; }
     public string FullName { get; set; }
+    public string Address { get; set; }
     public string PhoneNumber { get; set; }
 }
