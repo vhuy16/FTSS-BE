ba44d6d8c75a560327719da56f0d1e074a27d773
GiangNguyen <133587884+NooPhuocGiang@users.noreply.github.com>
2025-01-23 21:26:50 +0700

    fix CRUD voucher

---
+++ FTSS_API/Payload/Response/Voucher/VoucherResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace FTSS_API.Payload.Response.Voucher
+{
+    public class VoucherResponse
+    {
+        public Guid Id { get; set; }
+
+        public string VoucherCode { get; set; } = null!;
+
+        public decimal Discount { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public DateTime? ModifyDate { get; set; }
+
+        public bool? IsDelete { get; set; }
+
+        public string? Status { get; set; }
+
+        public int? Quantity { get; set; }
+
+        public decimal? MaximumOrderValue { get; set; }
+
+        public DateTime? ExpiryDate { get; set; }
+
+        public string? DiscountType { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
