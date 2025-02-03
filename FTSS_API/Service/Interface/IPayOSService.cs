53564ad03e7caaf86b847c692d6028686a211a80
vhuy16 <109493886+vhuy16@users.noreply.github.com>
2025-02-03 22:59:59 +0700

    fix webhook final

--- FTSS_API/Service/Interface/IPayOSService.cs
+++ FTSS_API/Service/Interface/IPayOSService.cs
@@ -10,6 +10,6 @@ public interface IPayOSService
     Task<ExtendedPaymentInfo> GetPaymentInfo(string paymentLinkId);
     Task<Result<PayOsService.PaymentLinkResponse>> CreatePaymentUrlRegisterCreator(Guid orderId);
     Task<ApiResponse> HandlePaymentCallback(string paymentLinkId, long orderCode);
-    Task<Result> HandlePayOsWebhook(WebhookType webhookBody);
+    Task<Result> HandlePayOsWebhook(WebhookNotification webhookBody);
     Task<ApiResponse> ConfirmWebhook(string webhookUrl);
 }
