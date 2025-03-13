0d059bf2878b3efab30ae4b0086309c478ec30f3
vhuy16 <109493886+vhuy16@users.noreply.github.com>
2025-03-13 23:41:32 +0700

    crud shipment and fix update order

---
+++ FTSS_API/Payload/Request/Shipment/ShipmentRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace FTSS_API.Payload.Request.Shipment;
+
+public class ShipmentRequest
+{
+    public Guid OrderId { get; set; }
+    public string? ShippingAddress { get; set; }
+    public decimal? ShippingFee { get; set; }
+    public string? DeliveryStatus { get; set; }
+    public string? TrackingNumber { get; set; }
+    public DateTime? DeliveryDate { get; set; }
+    public string? DeliveryAt { get; set; }
+}
