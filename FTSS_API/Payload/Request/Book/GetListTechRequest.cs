a781cdb7c8a7886c72cd0ca0529f3134c66709a1
GiangNguyen <133587884+NooPhuocGiang@users.noreply.github.com>
2025-03-22 14:28:55 +0700

    api get tech rảnh

---
+++ FTSS_API/Payload/Request/Book/GetListTechRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace FTSS_API.Payload.Request.Book
+{
+    public class GetListTechRequest
+    {
+        public DateTime? ScheduleDate { get; set; }
+    }
+}
--- FTSS_API/Payload/Response/Book/GetListMissionForManagerResponse.cs
+++ FTSS_API/Payload/Response/Book/GetListMissionForManagerResponse.cs
@@ -15,5 +15,8 @@
         public string? Address { get; set; }
 
         public string? PhoneNumber { get; set; }
+        public Guid? TechnicianId { get; set; }
+        public string TechnicianName { get; set; } = null!;
+        public Guid? BookingId { get; set; }
     }
 }
