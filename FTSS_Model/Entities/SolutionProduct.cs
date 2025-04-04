29d1fc9d383f42d4c791b31afe45eeb3448b3ce0
vhuy16 <109493886+vhuy16@users.noreply.github.com>
2025-04-04 18:25:34 +0700

    db

---
+++ FTSS_Model/Entities/SolutionProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FTSS_Model.Entities;
+
+public partial class SolutionProduct
+{
+    public Guid Id { get; set; }
+
+    public Guid SolutionId { get; set; }
+
+    public Guid ProductId { get; set; }
+
+    public DateTime? CreateDate { get; set; }
+
+    public DateTime? ModifyDate { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+
+    public virtual Solution Solution { get; set; } = null!;
+}
