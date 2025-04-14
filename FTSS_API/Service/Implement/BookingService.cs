c4a1bbd453403fb2efe6c2aa229ffd8790ffb7ea
GiangNguyen <133587884+NooPhuocGiang@users.noreply.github.com>
2025-04-14 20:31:32 +0700

    fix gethistoryorder

--- FTSS_API/Service/Implement/BookingService.cs
+++ FTSS_API/Service/Implement/BookingService.cs
@@ -1941,7 +1941,7 @@ namespace FTSS_API.Service.Implement
                 {
                     return new ApiResponse
                     {
-                        status = StatusCodes.Status404NotFound.ToString(),
+                        status = StatusCodes.Status200OK.ToString(),
                         message = "Không tìm thấy lịch sử đặt lịch cho đơn hàng này.",
                         data = null
                     };
