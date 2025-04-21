489a67c17cba9b9ce3c9e27c94a2d2d809792faf
vhuy16 <109493886+vhuy16@users.noreply.github.com>
2025-04-21 12:23:23 +0700

    chat feature

---
+++ FTSS_API/Payload/Request/Message/Message.cs
@@ -0,0 +1,30 @@
+﻿
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace FTSS_API.Models;
+
+[Table("messages")]
+public class Message : BaseModel
+{
+    [PrimaryKey("id")]
+    public long Id { get; set; }
+
+    [Column("user_id")]
+    public Guid UserId { get; set; }
+
+    [Column("room_id")]
+    public Guid? RoomId { get; set; }
+
+    [Column("username")]
+    public string Username { get; set; }
+
+    [Column("role")]
+    public string Role { get; set; }
+
+    [Column("text")]
+    public string Text { get; set; }
+
+    [Column("timestamp")]
+    public DateTime Timestamp { get; set; }
+}
