b6b9c76f529ddb4fce05b2458bf297da6d6e102c
vhuy16 <109493886+vhuy16@users.noreply.github.com>
2025-05-11 12:11:22 +0700

    fix getuser

--- FTSS_API/Service/Implement/UserService.cs
+++ FTSS_API/Service/Implement/UserService.cs
@@ -315,7 +315,7 @@ public class UserService : BaseService<UserService>, IUserService
                 Status = u.Status.ToString(),
                 DistrictId = u.DistrictId,
             },
-            predicate: u => u.Status.Equals(UserStatusEnum.Available.GetDescriptionFromEnum()),
+           
             page: page,
             size: size);
 
